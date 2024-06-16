055657c2b8028accf7a4b6a07d1c619b228c52cd
argelhr <argelhr@hotmail.com>
2024-06-16 16:38:20 -0300

    chore: removendo imports nao mais utilizados

--- src/main/java/br/edu/ifsul/cstsi/cinema/api/filmes/FilmeService.java
+++ src/main/java/br/edu/ifsul/cstsi/cinema/api/filmes/FilmeService.java
@@ -1,13 +1,11 @@
 package br.edu.ifsul.cstsi.cinema.api.filmes;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class FilmeService {
