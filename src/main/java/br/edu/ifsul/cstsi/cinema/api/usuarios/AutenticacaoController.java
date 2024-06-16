3567c4790b750d6f301a5f91d872780ba8a6bc47
argelhr <argelhr@hotmail.com>
2024-06-16 15:46:26 -0300

    Fix: Arrumando autenticação

--- src/main/java/br/edu/ifsul/cstsi/cinema/api/usuarios/AutenticacaoController.java
+++ src/main/java/br/edu/ifsul/cstsi/cinema/api/usuarios/AutenticacaoController.java
@@ -1,17 +1,23 @@
 package br.edu.ifsul.cstsi.cinema.api.usuarios;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/api/v1/login")
+@RestController
+@RequestMapping("api/v1/login")
 public class AutenticacaoController {
+    @Autowired
     private AuthenticationManager manager;
 
-    public ResponseEntity<String> login(UsuarioDTO usuarioDTO){
+    @PostMapping
+    public ResponseEntity login(@RequestBody UsuarioDTO usuarioDTO){
         var authenticationDTO = new UsernamePasswordAuthenticationToken(usuarioDTO.usuario(), usuarioDTO.senha());
         manager.authenticate(authenticationDTO);
         return ResponseEntity.ok().build();
