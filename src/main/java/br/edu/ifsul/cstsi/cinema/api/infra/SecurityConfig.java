2c612a86944a3cc53ca5ef5340f8e53602951d1d
argelhr <argelhr@hotmail.com>
2024-06-16 16:15:40 -0300

    feat: implementação de autenticação basica, junto das rotas

--- src/main/java/br/edu/ifsul/cstsi/cinema/api/infra/SecurityConfig.java
+++ src/main/java/br/edu/ifsul/cstsi/cinema/api/infra/SecurityConfig.java
@@ -1,17 +1,27 @@
 package br.edu.ifsul.cstsi.cinema.api.infra;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsServiceHttpBasic userDetailsServiceHttpBasic;
 
     @Bean
     public AuthenticationManager authenticatorManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -19,9 +29,24 @@ public class SecurityConfig {
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(HttpMethod.POST, "/api/v1/login").permitAll();
+                    req.requestMatchers("/api/v1/filmes").permitAll();
+                    req.anyRequest().authenticated();
+
+                })
+                .httpBasic(Customizer.withDefaults())
+                .userDetailsService(userDetailsServiceHttpBasic);
+        return http.build();
+    }
 
 }
